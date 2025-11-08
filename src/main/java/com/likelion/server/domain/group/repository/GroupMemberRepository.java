cfa84acf9a38c48326dc8f83070e756adbc288b4
EunJi <univ217@naver.com>
2025-11-09 07:27:52 +0900

    :sparkles: Feat: 그룹/멤버 레포지토리 쿼리 메서드 추가

--- src/main/java/com/likelion/server/domain/group/repository/GroupMemberRepository.java
+++ src/main/java/com/likelion/server/domain/group/repository/GroupMemberRepository.java
@@ -3,4 +3,6 @@ package com.likelion.server.domain.group.repository;
 import com.likelion.server.domain.group.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GroupMemberRepository extends JpaRepository<Member, Long> {}
+public interface GroupMemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByGroupIdAndUserId(Long groupId, Long userId);
+}
--- src/main/java/com/likelion/server/domain/group/repository/StudyGroupRepository.java
+++ src/main/java/com/likelion/server/domain/group/repository/StudyGroupRepository.java
@@ -3,6 +3,9 @@ package com.likelion.server.domain.group.repository;
 import com.likelion.server.domain.group.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudyGroupRepository extends JpaRepository<Group, Long> {
     boolean existsByGroupCode(String groupCode);
+    Group findByGroupCode(String groupCode);
 }
