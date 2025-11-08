f6a9055f4534956e9a89df9983e169712b301634
EunJi <univ217@naver.com>
2025-11-09 04:27:16 +0900

    :sparkles: Feat: Group, Member 레포지토리 추가

---
+++ src/main/java/com/likelion/server/domain/group/repository/StudyGroupRepository.java
@@ -0,0 +1,6 @@
+package com.likelion.server.domain.group.repository;
+
+import com.likelion.server.domain.group.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyGroupRepository extends JpaRepository<Group, Long> {}
