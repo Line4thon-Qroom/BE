b8a5e2d882b8a5545db07183a666c59953d86b1d
EunJi <univ217@naver.com>
2025-11-10 08:19:11 +0900

    :sparkles: Feat: QuizService 인터페이스에 getQuizWithQa 메서드 추가

--- src/main/java/com/likelion/server/domain/quiz/service/QuizService.java
+++ src/main/java/com/likelion/server/domain/quiz/service/QuizService.java
@@ -3,9 +3,15 @@ package com.likelion.server.domain.quiz.service;
 import com.likelion.server.domain.quiz.web.dto.CreateQuizRequest;
 import com.likelion.server.domain.quiz.web.dto.CreateQuizResponse;
 import com.likelion.server.domain.quiz.web.dto.QuizDetailResponse;
+import com.likelion.server.domain.quiz.web.dto.QuizQaResponse;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface QuizService {
     CreateQuizResponse createQuiz(Long userId, CreateQuizRequest request);
 
     QuizDetailResponse getQuizDetail(Long quizId);
+
+    // 3. 시험지+QA 게시판 통합 조회
+    @Transactional(readOnly = true) // 조회이므로 readOnly = true
+    QuizQaResponse getQuizWithQa(Long quizId);
 }
