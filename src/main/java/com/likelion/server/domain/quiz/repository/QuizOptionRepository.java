8b504f22e34dc2b939ea0b07fe90811fff1d7763
EunJi <univ217@naver.com>
2025-11-10 08:17:40 +0900

    :sparkles: Feat: 통합 조회용 레포지토리 추가/수정

---
+++ src/main/java/com/likelion/server/domain/quiz/repository/QuizOptionRepository.java
@@ -0,0 +1,14 @@
+package com.likelion.server.domain.quiz.repository;
+
+import com.likelion.server.domain.quiz.entity.QuizOption;
+import com.likelion.server.domain.quiz.entity.QuizQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuizOptionRepository extends JpaRepository<QuizOption, Long> {
+    // 질문 ID 기준 보기 조회
+    List<QuizOption> findAllByQuestion(QuizQuestion question);
+}
