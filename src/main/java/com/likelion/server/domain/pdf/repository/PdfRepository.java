54f40bc334efc5c12b2ddc9e449b7c078d7bb44b
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2025-11-10 00:31:18 +0900

    ✨ Feat: 그룹별 pdf 조회 API 구현

--- src/main/java/com/likelion/server/domain/pdf/repository/PdfRepository.java
+++ src/main/java/com/likelion/server/domain/pdf/repository/PdfRepository.java
@@ -3,4 +3,8 @@ package com.likelion.server.domain.pdf.repository;
 import com.likelion.server.domain.pdf.entity.Pdf;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PdfRepository extends JpaRepository<Pdf, Long> {}
+import java.util.List;
+
+public interface PdfRepository extends JpaRepository<Pdf, Long> {
+    List<Pdf> findAllByGroupId(Long groupId);
+}
