291e6577e48a41413859d4714d3539b733f078e5
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2025-11-10 03:24:15 +0900

    ✨ Feat: Quiz 생성 시 QA 게시판 자동 생성을 위한 임시 코드 작성

--- src/main/java/com/likelion/server/domain/qa/entity/QaBoard.java
+++ src/main/java/com/likelion/server/domain/qa/entity/QaBoard.java
@@ -1,5 +1,7 @@
 package com.likelion.server.domain.qa.entity;
 
+import com.likelion.server.domain.group.entity.Group;
+import com.likelion.server.domain.quiz.entity.Quiz;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,10 +11,29 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class QaBoard {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
     @Column(nullable = false)
     private String boardName;
+
+
+    // === Setter ===
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 }
