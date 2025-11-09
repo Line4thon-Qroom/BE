68a985de45925a11d15b232d75e48e698b153ed8
EunJi <univ217@naver.com>
2025-11-10 08:16:30 +0900

    :sparkles: Feat: 퀴즈 조회 실패용 커스텀 예외 추가

---
+++ src/main/java/com/likelion/server/domain/quiz/exception/QuizNotFoundException.java
@@ -0,0 +1,7 @@
+package com.likelion.server.domain.quiz.exception;
+
+import com.likelion.server.global.exception.BaseException;
+
+public class QuizNotFoundException extends BaseException {
+    public QuizNotFoundException() { super(QuizErrorCode.QUIZ_NOT_FOUND); }
+}
