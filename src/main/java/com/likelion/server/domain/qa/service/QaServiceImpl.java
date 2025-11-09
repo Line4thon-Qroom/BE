d826818be189cbda40190c06eb0f7808422b16ee
EunJi <univ217@naver.com>
2025-11-10 03:48:54 +0900

    :sparkles: Feat: QA 댓글 등록 서비스 로직 구현

--- src/main/java/com/likelion/server/domain/qa/service/QaServiceImpl.java
+++ src/main/java/com/likelion/server/domain/qa/service/QaServiceImpl.java
@@ -1,10 +1,15 @@
 package com.likelion.server.domain.qa.service;
 
 import com.likelion.server.domain.qa.entity.QaBoard;
+import com.likelion.server.domain.qa.entity.QaComment;
 import com.likelion.server.domain.qa.entity.QaPost;
 import com.likelion.server.domain.qa.exception.QaNotFoundException;
+import com.likelion.server.domain.qa.exception.QaPostNotFoundException;
 import com.likelion.server.domain.qa.repository.QaBoardRepository;
+import com.likelion.server.domain.qa.repository.QaCommentRepository;
 import com.likelion.server.domain.qa.repository.QaPostRepository;
+import com.likelion.server.domain.qa.web.dto.QaCommentRequest;
+import com.likelion.server.domain.qa.web.dto.QaCommentResponse;
 import com.likelion.server.domain.qa.web.dto.QaPostRequest;
 import com.likelion.server.domain.qa.web.dto.QaPostResponse;
 import com.likelion.server.domain.user.entity.User;
@@ -21,6 +26,7 @@ public class QaServiceImpl implements QaService {
 
     private final QaPostRepository qaPostRepository;
     private final QaBoardRepository qaBoardRepository;
+    private final QaCommentRepository qaCommentRepository;
     private final UserRepository userRepository;
 
     @Override
@@ -32,15 +38,15 @@ public class QaServiceImpl implements QaService {
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자를 찾을 수 없습니다: " + currentUserId));
 
         // QaBoard 조회
-        QaBoard board = qaBoardRepository.findById(request.getBoardId())
+        QaBoard board = qaBoardRepository.findById(request.boardId())
                 .orElseThrow(QaNotFoundException::new);
 
         // QaPost 엔티티 생성
         QaPost newPost = QaPost.builder()
                 .board(board)
                 .writer(currentUser)
-                .title(request.getTitle())
-                .content(request.getContent())
+                .title(request.title())
+                .content(request.content())
                 .build();
 
         // 엔티티 저장
@@ -48,5 +54,31 @@ public class QaServiceImpl implements QaService {
 
         // return
         return new QaPostResponse(savedPost);
+    }
+
+    @Override
+    @Transactional
+    public QaCommentResponse createComment(QaCommentRequest request, Long currentUserId) {
+
+        // 사용자 조회
+        User currentUser = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자를 찾을 수 없습니다: " + currentUserId));
+
+        // 부모 게시글(QaPost) 조회
+        QaPost parentPost = qaPostRepository.findById(request.postId())
+                .orElseThrow(QaPostNotFoundException::new);
+
+        // 댓글 엔티티 생성
+        QaComment newComment = QaComment.builder()
+                .post(parentPost)
+                .user(currentUser)
+                .content(request.content())
+                .build();
+
+        // 댓글 저장
+        QaComment savedComment = qaCommentRepository.save(newComment);
+
+        // Return
+        return new QaCommentResponse(savedComment);
     }
 }
