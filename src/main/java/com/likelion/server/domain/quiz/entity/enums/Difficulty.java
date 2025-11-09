ac7861982fb46958f4b7ce183635adc2d5211e69
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2025-11-10 05:14:50 +0900

    ⚡️ Refac: quiz 관련 response 코드 DTO 로 분할

--- src/main/java/com/likelion/server/domain/quiz/entity/enums/Difficulty.java
+++ src/main/java/com/likelion/server/domain/quiz/entity/enums/Difficulty.java
@@ -1,5 +1,22 @@
 package com.likelion.server.domain.quiz.entity.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Difficulty {
-    상, 중, 하
+    상("상"), 중("중"), 하("하");
+
+    private final String korean;
+
+    public static Difficulty fromKorean(String value) {
+        return switch (value) {
+            case "상" -> 상;
+            case "중" -> 중;
+            case "하" -> 하;
+            default -> throw new IllegalArgumentException("잘못된 난이도 값: " + value);
+        };
+    }
 }
+
