e3328e5c3f0bb6193b45258c6465bf942b62d508
EunJi <univ217@naver.com>
2025-11-11 01:55:52 +0900

    :art: Add: PATCH 메서드 추가

--- src/main/java/com/likelion/server/global/config/WebConfig.java
+++ src/main/java/com/likelion/server/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "https://likelion13-changuphalgak.netlify.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
 }
