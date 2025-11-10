cc877525c540260aa0a46adfa81ff850dcf27da9
EunJi <univ217@naver.com>
2025-11-10 11:14:53 +0900

    :sparkles: Feat: 홈 화면 조회에 필요한 레포지토리 메서드 추가

--- src/main/java/com/likelion/server/domain/quiz/repository/QuizRepository.java
+++ src/main/java/com/likelion/server/domain/quiz/repository/QuizRepository.java
@@ -1,8 +1,13 @@
 package com.likelion.server.domain.quiz.repository;
 
+import com.likelion.server.domain.group.entity.Group;
 import com.likelion.server.domain.quiz.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
     int countByPdfId(Long pdfId);
+
+    List<Quiz> findAllByGroup(Group group);
 }
