c856b7a63e79586ba118836640923d45acdccddd
EunJi <univ217@naver.com>
2025-11-12 01:55:53 +0900

    :sparkles: Feat: 그룹 스터디룸 메인화면 조회 API 구현

--- src/main/java/com/likelion/server/domain/group/service/GroupService.java
+++ src/main/java/com/likelion/server/domain/group/service/GroupService.java
@@ -3,10 +3,12 @@ package com.likelion.server.domain.group.service;
 import com.likelion.server.domain.group.web.dto.CreateGroupRequest;
 import com.likelion.server.domain.group.web.dto.CreateGroupResponse;
 import com.likelion.server.domain.group.web.dto.GroupJoinResponse;
+import com.likelion.server.domain.group.web.dto.GroupMainResponse;
 
 public interface GroupService {
     CreateGroupResponse create(Long userId, CreateGroupRequest req);
     GroupJoinResponse joinByCode(Long userId, String groupCode);
     void leaveGroup(Long userId, Long groupId);
     void deleteGroup(Long userId, Long groupId);
+    GroupMainResponse getGroupMain(Long userId, Long groupId);
 }
