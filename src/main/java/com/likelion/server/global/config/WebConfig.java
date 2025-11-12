9e4d8559cd6af837a9a52c6ef5a4b52367156a09
EunJi <univ217@naver.com>
2025-11-12 22:44:11 +0900

    :wrench: Feat: 프론트 배포 주소 CORS 허용

--- src/main/java/com/likelion/server/global/config/WebConfig.java
+++ src/main/java/com/likelion/server/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://likelion13-changuphalgak.netlify.app")
+                .allowedOrigins("http://localhost:5173", "https://4thon-qroom.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
